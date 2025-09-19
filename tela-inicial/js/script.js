3da9e8ef0fb8dab2af2a646ccf32849e6838ba99
5456ju00 <julia.caroline.goncalves@escola.pr.gov.br>
2025-09-19 08:20:55 -0300

    arrumando as telas

--- tela-inicial/js/script.js
+++ tela-inicial/js/script.js
@@ -1,6 +1,11 @@
 // Exemplo simples de interação com JavaScript
 document.querySelectorAll('a').forEach(link => {
     link.addEventListener('click', function(event) {
-        alert('Link clicado: ' + event.target.textContent);
+      
+
+        window.location.href = "sucesso.html"
+
     });
+
+    
 });
