00063f32d9eb38924b0c606948c8f6296130eb77
ADCR14 <a.rodrigues14@escola.pr.gov.br>
2025-09-23 11:38:09 -0300

    login ok

--- login/js/script.js
+++ login/js/script.js
@@ -1,35 +1,78 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const form = document.getElementById('formLogin');
-    const mensagemDiv = document.getElementById('mensagem');
-    const resetButton = document.getElementById('resetButton');
+function salvarLogin(){
 
-    form.addEventListener('submit', function (e) {
-        e.preventDefault();
+    const usuario = document.getElementById("usuario").value.trim();
+    const senha = document.getElementById("senha").value.trim();
+    const telefone = document.getElementById("telefone").value.trim();
 
-        const usuario = document.getElementById('usuario').value;
-        const senha = document.getElementById('senha').value;
-        const email = document.getElementById('email').value;
+    var headers= new Headers();
+    headers.append("Content-Type", "application/json");
+    headers.append("Access-Control-Allow-Origin", "*");
 
-        // Verificação simples para garantir que os campos estão preenchidos
-        if (!usuario || !senha || !email) {
-            showMessage("Por favor, preencha todos os campos.", "error");
-            return;
+    fetch( "http://127.0.0.1:8080/login/cadlogin"  ,{
+        method: "POST", 
+        mode: "cors",
+        cache: "no-store",
+
+        body: JSON.stringify({
+            usuario: usuario,
+            senha: senha,
+            telefone: telefone
+        }),
+
+        headers: headers
+
+
+    }).then(  response =>  {
+        if ( !response.ok){
+            throw new Error("Erro na resposta da API")
         }
+        return response.json(); // converte o corpo da resposta em JSON
+    }).then(  data =>  {
 
-        // Simulação de envio de dados
-        setTimeout(() => {
-            showMessage("Login realizado com sucesso!", "success");
-        }, 500);
-    });
+        const login_id = data.id;
+        console.log("Id do registro salvo: ", login_id);
 
-    resetButton.addEventListener('click', () => {
-        form.reset();
-        mensagemDiv.innerHTML = '';
-    });
 
-    window.location.href = 'sucesso.html'; 
-    function showMessage(message, type) {
-        mensagemDiv.innerHTML = message;
-        mensagemDiv.className = 'mensagem ' + type;
-    }
-});
+        localStorage.setItem('id_login', login_id);
+
+    }).catch(error => console.error('Erro!:', error));
+
+}
+
+
+
+// document.addEventListener("DOMContentLoaded", () => {
+//     const form = document.getElementById('formLogin');
+//     const mensagemDiv = document.getElementById('mensagem');
+//     const resetButton = document.getElementById('resetButton');
+
+//     form.addEventListener('submit', function (e) {
+//         e.preventDefault();
+
+//         const usuario = document.getElementById('usuario').value;
+//         const senha = document.getElementById('senha').value;
+//         const email = document.getElementById('email').value;
+
+//         // Verificação simples para garantir que os campos estão preenchidos
+//         if (!usuario || !senha || !email) {
+//             showMessage("Por favor, preencha todos os campos.", "error");
+//             return;
+//         }
+
+//         // Simulação de envio de dados
+//         setTimeout(() => {
+//             showMessage("Login realizado com sucesso!", "success");
+//         }, 500);
+//     });
+
+//     resetButton.addEventListener('click', () => {
+//         form.reset();
+//         mensagemDiv.innerHTML = '';
+//     });
+
+//     window.location.href = 'sucesso.html'; 
+//     function showMessage(message, type) {
+//         mensagemDiv.innerHTML = message;
+//         mensagemDiv.className = 'mensagem ' + type;
+//     }
+// });
