159782865b0ee355f6ec07fdcb434acfb0c66604
LckzUp <lucas.pinto17@escola.pr.gov.br>
2025-09-23 11:17:01 -0300

    ajustado o js do orcamento

--- orcamento/js/script.js
+++ orcamento/js/script.js
@@ -1,33 +1,80 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const form = document.getElementById('formOrcamento');
-  const mensagemDiv = document.getElementById('mensagem');
-  const resetButton = document.getElementById('resetButton');
+function salvarOrcamento(){
 
-  form.addEventListener('submit', function (e) {
-      e.preventDefault();
+    const maoDeObra = document.getElementById("maoDeObra").value.trim();
+    const produto = document.getElementById("produto").value.trim();
+    const codigoOrcamento = document.getElementById("codigoOrcamento").value.trim();
 
-      const maoDeObra = document.getElementById('maoDeObra').value;
-      const produto = document.getElementById('produto').value;
-      const codigoOrcamento = document.getElementById('codigoOrcamento').value;
 
-      if (!maoDeObra || !produto || !codigoOrcamento) {
-          showMessage("Por favor, preencha todos os campos corretamente.", "error");
-          return;
-      }
+    var headers= new Headers();
+    headers.append("Content-Type", "application/json");
+    headers.append("Access-Control-Allow-Origin", "*");
 
-      // Simulação de envio para o backend ou armazenamento de dados
-      setTimeout(() => {
-          showMessage("Orçamento cadastrado com sucesso!", "success");
-      }, 500);
-  });
+    fetch( "http://127.0.0.1:8080/orcamento/cadorca"  ,{
+        method: "POST", 
+        mode: "cors",
+        cache: "no-store",
 
-  resetButton.addEventListener('click', () => {
-      form.reset();
-      mensagemDiv.innerHTML = '';
-  });
+        body: JSON.stringify({
+            maoDeObra: maoDeObra,
+            produto: produto,
+            codigoOrcamento: codigoOrcamento
+        }),
 
-  function showMessage(message, type) {
-      mensagemDiv.innerHTML = message;
-      mensagemDiv.className = 'mensagem ' + type;
-  }
-});
+        headers: headers
+
+
+    }).then(  response =>  {
+        if ( !response.ok){
+            throw new Error("Erro na resposta da API")
+        }
+        return response.json(); // converte o corpo da resposta em JSON
+    }).then(  data =>  {
+
+        const orcamento_id = data.id;
+        console.log("Id do registro salvo: ", orcamento_id);
+
+
+        localStorage.setItem('id_orcamento', orcamento_id);
+
+
+    }).catch(error => console.error('Erro!:', error));
+}
+
+
+// function salvarOrcamento(){
+
+//     document.addEventListener("DOMContentLoaded", () => {
+//     const form = document.getElementById('formOrcamento');
+//     const mensagemDiv = document.getElementById('mensagem');
+//     const resetButton = document.getElementById('resetButton');
+
+//     form.addEventListener('submit', function (e) {
+//         e.preventDefault();
+
+//         const maoDeObra = document.getElementById('maoDeObra').value;
+//         const produto = document.getElementById('produto').value;
+//         const codigoOrcamento = document.getElementById('codigoOrcamento').value;
+
+//         if (!maoDeObra || !produto || !codigoOrcamento) {
+//             showMessage("Por favor, preencha todos os campos corretamente.", "error");
+//             return;
+//         }
+
+//         // Simulação de envio para o backend ou armazenamento de dados
+//         setTimeout(() => {
+//             showMessage("Orçamento cadastrado com sucesso!", "success");
+//         }, 500);
+//     });
+
+//     resetButton.addEventListener('click', () => {
+//         form.reset();
+//         mensagemDiv.innerHTML = '';
+//     });
+
+//     function showMessage(message, type) {
+//         mensagemDiv.innerHTML = message;
+//         mensagemDiv.className = 'mensagem ' + type;
+//     }
+//     });
+
+// }
